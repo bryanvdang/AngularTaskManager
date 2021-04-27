8029b4b03cf06b4721e8ba9882043ed11ed0a9bc
Bryan Dang <bryanvdang@gmail.com>
2021-04-26 19:58:34 -0700

    created dashboard page

--- TaskManager/src/app/dashboard/dashboard.component.ts
+++ TaskManager/src/app/dashboard/dashboard.component.ts
@@ -18,6 +18,13 @@ export class DashboardComponent implements OnInit
   CurrentExpenditure: number;
   AvailableFunds: number;
 
+  Clients: string[];
+  Projects: string[];
+  Years: number[] = [];
+  TeamMembersSummary = [];
+  TeamMembers = [];
+
+
   ngOnInit() {
      this.Designation = "Team Leader"; //"this" initializes the value
      this.Username = "Jonny Appleseed";
@@ -28,6 +35,52 @@ export class DashboardComponent implements OnInit
      this.ProjectCost = 2113507;
      this.CurrentExpenditure = 96788;
      this.AvailableFunds = 52536;
+
+     this.Clients = [
+       "ABC Infotech Ltd.", "DEF Software Solutions", "GHI Industries"
+     ]
+
+     this.Projects = [
+       "Project A", "Project B", "Project C", "Project D"
+     ]
+
+     for(var i=2019; i >= 2010; i--) {
+      this.Years.push(i);
+     }
+
+     this.TeamMembersSummary = [
+       {Region: "EAST", TeamMembersCount: 20, TemporarilyUnavailableMembers: 4},
+       {Region: "SOUTH", TeamMembersCount: 15, TemporarilyUnavailableMembers: 8},
+       {Region: "WEST", TeamMembersCount: 17, TemporarilyUnavailableMembers: 1},
+       {Region: "NORTH", TeamMembersCount: 15, TemporarilyUnavailableMembers: 6},
+     ]
+
+     this.TeamMembers = [
+       {Region:"East", Members: [
+        {ID: 1, Name: "Ford", Status: "Available"},
+        {ID: 2, Name: "Miller", Status: "Available"},
+        {ID: 3, Name: "Jones", Status: "Busy"},
+        {ID: 4, Name: "James", Status: "Busy"}
+       ]},
+       {Region:"South", Members: [
+        {ID: 5, Name: "Bryan", Status: "Available"},
+        {ID: 6, Name: "Adam", Status: "Available"},
+        {ID: 7, Name: "David", Status: "Busy"},
+        {ID: 8, Name: "Jess", Status: "Busy"}
+       ]},
+       {Region:"West", Members: [
+        {ID: 9, Name: "Duke", Status: "Available"},
+        {ID: 10, Name: "Tyler", Status: "Available"},
+        {ID: 11, Name: "John", Status: "Busy"},
+        {ID: 12, Name: "Ashton", Status: "Busy"}
+       ]},
+       {Region:"North", Members: [
+        {ID: 13, Name: "Chad", Status: "Available"},
+        {ID: 14, Name: "Kyle", Status: "Available"},
+        {ID: 15, Name: "Monica", Status: "Busy"},
+        {ID: 16, Name: "Becky", Status: "Busy"}
+       ]},
+      ];
   }
 
 }
