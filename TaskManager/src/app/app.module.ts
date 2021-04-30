1b8bf2e095ab5b5e56e75b8fea70f21a7862be2b
Bryan Dang <bryanvdang@gmail.com>
2021-04-29 22:13:28 -0700

    created admin module and userprofile componenet

--- TaskManager/src/app/app.module.ts
+++ TaskManager/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, //predfined code for DOM manipulation in the browser
+    AppRoutingModule, //whatever routes we created in the appRoutingModule, are loaded in the app module
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
